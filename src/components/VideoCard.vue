31203a232524f6660e24d3f518b65306edf20b86
Arnaud <arrnicel@gmail.com>
2023-11-02 22:04:59 +0100

    Add download with API v3

--- src/components/VideoCard.vue
+++ src/components/VideoCard.vue
@@ -16,6 +16,12 @@
       </div>
     </v-card-text>
     <v-card-actions class="pt-0">
+      <v-btn icon :loading="loading_video" @click="load_video" :href="`${stc.afr.v3}/youtube/download/video/${id}`">
+        <v-icon color="red">mdi-download-box</v-icon>
+      </v-btn>
+      <v-btn icon :loading="loading_audio" @click="load_audio" :href="`${stc.afr.v3}/youtube/download/audio/${id}`" style="margin-left: 0;">
+        <v-icon color="red">mdi-download-box-outline</v-icon>
+      </v-btn>
       <v-spacer></v-spacer>
       <v-btn icon @click.stop="$emit('showComment', id)">
         <v-icon color="red">mdi-message-text-fast</v-icon>
@@ -25,7 +31,23 @@
 </template>
 
 <script>
+  import stc from '../plugins/static';
   export default {
     props: ['title','source','id'],
+    data: () => ({
+      stc: stc,
+      loading_video: false,
+      loading_audio: false,
+    }),
+    methods: {
+      load_video() {
+        this.loading_video = true
+        setTimeout(() => (this.loading_video = false), 10000)
+      },
+      load_audio() {
+        this.loading_audio = true
+        setTimeout(() => (this.loading_audio = false), 10000)
+      },
+    },
   }
 </script>
--- src/plugins/static.js
+++ src/plugins/static.js
@@ -1,10 +1,11 @@
 export default {
     afr: {
         api: 'https://api.arfevrier.fr/v2',
+        v3: 'https://api.arfevrier.fr/v3',
     },
     yt: {
         api: 'https://www.googleapis.com/youtube/v3',
         thumbnail: 'https://i.ytimg.com/vi',
-        key: 'AIzaSyDvXwykt34G-Ebxa1kNyDCqAuAo0Jj6J5k'
+        key: 'AIzaSyDvXwykt34G-Ebxa1kNyDCqAuAo0Jj6J5k',
     },
 }
